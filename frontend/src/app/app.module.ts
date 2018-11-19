5850c81fbcd8e1547d0cb2f2c481bad86d7e5e5f
munHunger <marcusmunger@hotmail.com>
2018-11-19 05:59:42 +0100

    added class component and path

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,11 +7,15 @@ import { ChatComponent } from "./components/chat/chat.component";
 import { MessageComponent } from "./components/chat/message/message.component";
 import { ChatService } from "./services/chat.service";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { ClassComponent } from "./components/class/class.component";
 
-const appRoutes: Routes = [{ path: "chat", component: ChatComponent }];
+const appRoutes: Routes = [
+  { path: "chat", component: ChatComponent },
+  { path: "class", component: ClassComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent, ChatComponent, MessageComponent],
+  declarations: [AppComponent, ClassComponent, ChatComponent, MessageComponent],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes), HttpClientModule],
   providers: [ChatService],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/class/class.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "class",
+  templateUrl: "./class.component.html",
+  styleUrls: ["./class.component.css"]
+})
+export class ClassComponent {
+  constructor() {}
+}
